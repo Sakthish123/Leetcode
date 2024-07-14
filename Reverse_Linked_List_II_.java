9f9a28ce1d72aa61293494773200bf93a7f7de9a
Sakthish123 <140630593+Sakthish123@users.noreply.github.com>
2024-07-14 17:37:27 +0530

    Update and rename Reverse_Linked_List.java to Reverse_Linked_List_II_.java

--- Reverse_Linked_List_II_.java
+++ Reverse_Linked_List_II_.java
@@ -9,19 +9,37 @@
  * }
  */
 class Solution {
-    
-    public ListNode reverseList(ListNode head) {
+    public ListNode reverse(ListNode head,int right){
         ListNode H=head;
         while(true){
           if(head==null)
             break;
           if(head.next==null)
             break;
+          if(right==1)
+            break;
           ListNode h=head.next;
           head.next=head.next.next;
           h.next=H;
           H=h;
+          right--;
         }
+        return H;
+    }
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(left==right)
+          return head;
+        if(left==1)
+          return reverse(head,right);
+        ListNode H=head,h=head;
+        while(left!=1){
+            h=head;
+            head=head.next;
+            left--;
+            right--;
+        }
+        head=reverse(head,right);
+        h.next=head;
         return H;
     }
 }
