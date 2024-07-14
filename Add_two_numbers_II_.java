9957ff04fb662087d4cea1e709bffc6c6abdbe1b
Sakthish123 <140630593+Sakthish123@users.noreply.github.com>
2024-07-14 22:13:32 +0530

    adding two numbers(Linked List) whose digits are stored in nodes of a linked list and the first digit is present at the head

---
+++ Add_two_numbers_II_.java
@@ -0,0 +1,75 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+ class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode H=head;
+        while(true){
+          if(head==null)
+            break;
+          if(head.next==null)
+            break;
+          ListNode h=head.next;
+          head.next=head.next.next;
+          h.next=H;
+          H=h;
+        }
+        return H;
+    }
+    public int count(ListNode head){
+        int c=0;
+        while(true){
+            if(head==null)
+             break;
+            c++;
+        }
+       return c;
+    }
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        l1=reverseList(l1);
+        l2=reverseList(l2);
+        ListNode t=l1;
+        ListNode h=l1;
+        int c=0;
+        while(l2!=null){
+            l1.val=l1.val+l2.val+c;
+            if(l1.val>9){
+                c=1;
+                l1.val%=10;
+            }
+            else
+             c=0;
+            if(l1.next==null&&l2.next!=null){
+                l1.next=l2.next;
+                l2.next=null;
+            }
+            t=l1;
+            l1=l1.next;
+            l2=l2.next;
+        }
+        while(c!=0&&l1!=null){
+            l1.val=l1.val+c;
+            if(l1.val>9){
+                c=1;
+                l1.val%=10;
+            }
+            else
+             c=0;
+            t=l1;
+            l1=l1.next;
+        }
+        if(c!=0&&l1==null){
+            ListNode L=new ListNode(c,null);
+            t.next=L;
+        }
+          
+        return reverseList(h);
+    }
+}
